a15cca758fb606c95946db23907cf3794ea9e64a
Patrick4Ndolo <ndolopatrick52@gmail.com>
2022-12-23 11:38:35 +0000

    _getline.c

--- _getline.c
+++ _getline.c
@@ -7,7 +7,7 @@
 *
 * Return: reading counting bytes.
 */
-int _getline(data_of_program * data)
+int _getline(data_of_program  *data)
 {
 	char buff[BUFFER_SIZE] = {'\0'};
 	static char *array_commands[10] = {NULL};
